e7f65ed29a715e665aeeaec647019b7203b07340
MERCURY <wanasart2@gmail.com>
2024-06-25 00:14:31 +0700

    Update: 25/06/67
    Time: 00:14

--- Assets/Scripts/CountStart.cs
+++ Assets/Scripts/CountStart.cs
@@ -11,6 +11,11 @@ public class CountStart : MonoBehaviour
     public GameObject GameObj1;
     public GameObject GameObj2;
 
+    [Header("Sound")]
+    public AudioClip countSound;
+
+    private AudioSource audioSource;
+
     private void Awake()
     {
         if (Instance == null)
@@ -27,6 +32,10 @@ public class CountStart : MonoBehaviour
     {
         // เริ่มต้นนับถอยหลังได้โดยการเรียก StartCountdown()
         GameObj2.SetActive(false);
+
+        audioSource = GetComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        audioSource.Stop();
     }
 
     public void StartCountdown()
@@ -40,6 +49,8 @@ public class CountStart : MonoBehaviour
     {
         int countdownTime = 3;
 
+        audioSource.clip = countSound;
+        audioSource.Play();
         while (countdownTime > 0)
         {
             tmpCountStart.text = countdownTime.ToString();
@@ -53,7 +64,7 @@ public class CountStart : MonoBehaviour
         tmpCountStart.text = "START"; // Display "START"
         tmpCountStart.fontSize = 300; // Set font size to 300
         tmpCountStart.transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
-        yield return new WaitForSeconds(2.0f); // Wait for 2 seconds
+        yield return new WaitForSeconds(1.25f); // Wait for 2 seconds
         tmpCountStart.transform.localScale = Vector3.one; // Reset to original scale
         tmpCountStart.fontSize = 100; // Reset font size to default (change 100 to the original font size if different)
 
